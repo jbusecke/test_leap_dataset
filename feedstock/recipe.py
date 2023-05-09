0ba0f255cc2c029561ba7c3486200a291eee8cad
jbusecke <julius@ldeo.columbia.edu>
2023-05-09 16:32:23 -0400

    Remove hardcoded paths and move to config

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -6,20 +6,47 @@ from pangeo_forge_recipes.patterns import pattern_from_file_sequence
 from pangeo_forge_recipes.transforms import (
     OpenURLWithFSSpec, OpenWithXarray, StoreToZarr
     )
+transforms_dict = {}
+
+# for testing
+years = range(2001, 2003)
+# years = range(2001, 2021)
+
+#trying to avoid time out errors
+# see https://stackoverflow.com/a/73746020
+from aiohttp import ClientTimeout
+open_kwargs = {"client_kwargs":{"timeout": ClientTimeout(total=5000, connect=1000)}}
+
+## Monthly version
 
 input_urls = [
-    'https://zenodo.org/record/7761881/files/METAFLUX_GPP_RECO_daily_200101.nc?download=1',
-    'https://zenodo.org/record/7761881/files/METAFLUX_GPP_RECO_daily_200102.nc?download=1',
+    f'https://zenodo.org/record/7761881/files/METAFLUX_GPP_RECO_monthly_{year}.nc?download=1' for year in years
 ]
 
 pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
 transforms = (
     beam.Create(pattern.items())
-    | OpenURLWithFSSpec()
+    | OpenURLWithFSSpec(open_kwargs=open_kwargs)
     | OpenWithXarray()
     | StoreToZarr(
-        store_name="test_leap_dataset.zarr",
+        store_name="METAFLUX_GPP_RECO_monthly.zarr",
         combine_dims=pattern.combine_dim_keys,
-        target_root="gs://leap-persistent/jbusecke/data/library/test",
     )
 )
+transforms_dict['METAFLUX_GPP_RECO_monthly'] = transforms
+
+
+## daily version
+input_urls = [f"https://zenodo.org/record/7761881/files/METAFLUX_GPP_RECO_daily_{year}{month:02}.nc?download=1" for year in years for month in range(1,12) ]
+pattern = pattern_from_file_sequence(input_urls, concat_dim='time')
+transforms = (
+    beam.Create(pattern.items())
+    | OpenURLWithFSSpec(open_kwargs=open_kwargs)
+    | OpenWithXarray()
+    | StoreToZarr(
+        store_name="METAFLUX_GPP_RECO_daily.zarr",
+        combine_dims=pattern.combine_dim_keys,
+    )
+)
+transforms_dict['METAFLUX_GPP_RECO_daily'] = transforms
+
